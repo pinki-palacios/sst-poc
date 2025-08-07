cda8305c9b27d5e4a8d8a96707311f62e41da40f
Luis Palacios <lpalacios.dev@gmail.com>
2025-08-07 17:51:32 -0300

    fix: autodeploy

--- infra/api.ts
+++ infra/api.ts
@@ -1,3 +1,4 @@
+import { Resource } from "sst";
 import { bucket } from "./storage";
 
 const apexDomain = `lapalacios.com`;
@@ -5,7 +6,15 @@ const routerDomain = `*.${apexDomain}`;
 const baseUrl = `sst-poc.${apexDomain}`;
 
 const router = new sst.aws.Router("MyRouter", {
-  domain: routerDomain,
+  domain:
+    $app.stage === "luispalacios"
+      ? routerDomain
+      : {
+          name: routerDomain,
+          aliases: [`www.${baseUrl}`],
+          dns: false,
+          cert: "arn:aws:acm:us-east-1:251724005896:certificate/c3a03e95-4424-4915-adad-d73b4bb56a2c",
+        },
 });
 
 const params = new sst.Linkable("Params", {
