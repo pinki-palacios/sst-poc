c3c504e7cd87cc60c1cb1fd2571c2191b06e85c5
Luis Palacios <38508418+pinki-palacios@users.noreply.github.com>
2025-08-07 19:16:11 -0300

    feat(ci): autodeploy (#1)
    * feat(ci): autodeploy
    
    * feat(ci): autodeploy
    
    * fix: autodeploy
    
    * fix: remove aliases
    
    * fix: cert
    
    * fix: maybe?
    
    * fix: set up cert
    
    * fix: set up cert
    
    * fix: reuse router

--- infra/api.ts
+++ infra/api.ts
@@ -1,7 +1,36 @@
+import { Resource } from "sst";
 import { bucket } from "./storage";
 
+const apexDomain = `lapalacios.com`;
+const routerDomain = `*.${apexDomain}`;
+const baseUrl = `sst-poc.${apexDomain}`;
+
+const router =
+  $app.stage === "luispalacios"
+    ? new sst.aws.Router("MyRouter", {
+        domain: {
+          name: routerDomain,
+          dns: false,
+          cert: "arn:aws:acm:us-east-1:251724005896:certificate/402a6ccc-efd8-4202-b231-02bd571492d9",
+        },
+      })
+    : sst.aws.Router.get("MyRouter", "E3V044MW450TLD");
+
+const params = new sst.Linkable("Params", {
+  properties: {
+    STAGE: $app.stage,
+    appName: $app.name,
+  },
+});
+
 export const myApi = new sst.aws.Function("MyApi", {
-  url: true,
-  link: [bucket],
-  handler: "packages/functions/src/api.handler"
+  url: {
+    router: {
+      domain: baseUrl,
+      instance: router,
+      path: `/api/${$app.stage}`,
+    },
+  },
+  link: [params, bucket],
+  handler: "packages/functions/src/api.handler",
 });
